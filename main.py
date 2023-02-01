585733db1e5b416a7f982ee68ac8d8d8e5448704
lMrSl <apalenqu22@jaumebalmes.net>
2023-02-01 20:36:05 +0100

    main

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from Sebastian import exercici1_A as ex1a
+from Sebastian import exercici2_A as ex2a
+
 from guifre import exercici1_B as ex1b
 from guifre import exercici2_B as ex2b
+
+ex1a.edad()
+ex2a.func(mates = 10, física = 9, química = 7)
 
 print("Exercici 1 B")
 ex1b.anyNascut(int(input("Escriu quants anys fas aquest any \n")))
